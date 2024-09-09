3ad9066e540e0c5973290724ab79be7ede26eeec
jesusroa10 <jesusroa297@gmail.com>
2024-09-09 14:57:24 -0500

    desaparece mensaje 2seg

--- webhook.js
+++ webhook.js
@@ -2,16 +2,23 @@ const getButton = document.getElementById('btn');
 const getLink = document.getElementById('link');
 const getDiv = document.getElementById('enviado');
 
-    getButton.addEventListener('click', () =>{
-        fetch('https://hook.us1.make.com/yj76ww3gsycomaaodr5h9911ujou458o', {
-            method: 'POST',
-            body: new URLSearchParams({
-              Link: getLink.value,
-              Message: getDiv.innerText
-        })
-
-      })
-    
-      .then(response => response.text()).then(envio => {getDiv.style.display='block'});
-      getLink.value = '';
+getButton.addEventListener('click', () => {
+  fetch('https://hook.us1.make.com/yj76ww3gsycomaaodr5h9911ujou458o', {
+    method: 'POST',
+    body: new URLSearchParams({
+      Link: getLink.value,
+      Message: getDiv.innerText
     })
+  })
+  .then(response => response.text())
+  .then(envio => {
+    getDiv.style.display = 'block';
+    
+    // Desaparecer el mensaje después de 2 seg.
+    setTimeout(() => {
+      getDiv.style.display = 'none';
+    }, 2000);
+  });
+  
+  getLink.value = '';
+});
